955515b8c5c8ad62ef0a3d2f33ad8e6df9a98992
Chris Dombele <christopher.dombele@gmail.com>
2019-07-10 23:59:40 -0400

    created search results page

---
+++ searchResults.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("main .col-lg").hover(
+    function() {
+      $(this).animate(
+        {
+          marginTop: "-=1%"
+        },
+        200
+      );
+    },
+    function() {
+      $(this).animate(
+        {
+          marginTop: "0%"
+        },
+        200
+      );
+    }
+  );
+});
