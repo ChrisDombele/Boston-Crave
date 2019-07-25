3ab25be8aff5b370f1fe70bc748d63e5778224ae
Chris Dombele <christopher.dombele@gmail.com>
2019-07-25 17:18:50 -0400

    Attempting to fetch api data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const URL = "https://api.yelp.com/v3/businesses/search";
+
+const config = {
+  headers: {
+    Authorization:
+      "EElj7yTkL-mqWepweO0Pgb-DQa_Yh9hsbvKPAKFB7dyFcDfAM5yn-a-0TH5I0eqbD8CA8RpbaIenoJ-Jw3Q33OOxz4mz6uct9H-ukaO7HpQXYYkV1Qd0TzDI95E4XXYx"
+  },
+  params: {
+    term: "tacos",
+    location: "Boston"
+  }
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: ""
+    };
+  }
+
+  handleClick() {
+    axios
+      .get(URL, config)
+      .then(response => {
+        // If request is good...
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log("error 3 " + error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="button__container">
+        <button className="button" onClick={this.handleClick}>
+          Click Me
+        </button>
+      </div>
+    );
+  }
 }
-
 export default App;
