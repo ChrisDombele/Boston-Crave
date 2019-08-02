e6d482a0a04dbc1ad583f0c7124a73498e86e978
Chris Dombele <christopher.dombele@gmail.com>
2019-08-01 22:48:21 -0400

    implemented react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 const URL = "https://api.yelp.com/v3/businesses/search";
@@ -19,17 +23,16 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      username: ""
+      data: []
     };
   }
 
-  handleClick() {
+  componentDidMount() {
     axios
       .get(proxyurl + URL, config)
-
       .then(response => {
-        // If request is good...
-        console.log(response);
+        this.setState({ data: response.data.businesses });
+        console.log(this.state.data);
       })
       .catch(error => {
         console.log("error 3 " + error);
@@ -38,11 +41,28 @@ class App extends Component {
 
   render() {
     return (
-      <div className="button__container">
-        <button className="button" onClick={this.handleClick}>
-          Click Me
-        </button>
-      </div>
+      <Container>
+        {this.state.data.map(data => (
+          <Row>
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Body>
+                  <Card.Title>{data.name}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    Card Subtitle
+                  </Card.Subtitle>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Card.Link href="#">Card Link</Card.Link>
+                  <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        ))}
+      </Container>
     );
   }
 }
