b4055f3c074cf502d8f60d3b9f7aeada0f53b2d7
Charlie Pluk <charliepluk@users.noreply.github.com>
2019-08-02 20:29:55 -0400

    Fixed Stars

--- src/SearchResultsNav.js
+++ src/SearchResultsNav.js
@@ -31,15 +31,11 @@ class SearchResultsNav extends Component {
                 <span>Rating:</span>
                 <br />
                 <select className="drop">
-                  <option value="1">&#128970;</option>
-                  <option value="2">&#128970;&#128970;</option>
-                  <option value="3">&#128970;&#128970;&#128970;</option>
-                  <option value="4">
-                    &#128970;&#128970;&#128970;&#128970;
-                  </option>
-                  <option value="5">
-                    &#128970;&#128970;&#128970;&#128970;&#128970;
-                  </option>
+                  <option value="1">✭</option>
+                  <option value="2">✭✭</option>
+                  <option value="3">✭✭✭</option>
+                  <option value="4">✭✭✭✭</option>
+                  <option value="5">✭✭✭✭✭</option>
                 </select>
               </Col>
               <Col className="col-lg text-center">
