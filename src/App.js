4cfe068e3e290968aa9a7f83b6b42d30f9ca5501
Chris Dombele <christopher.dombele@gmail.com>
2019-08-04 12:39:12 -0400

    adding changes

--- src/App.js
+++ src/App.js
@@ -26,83 +26,40 @@ import HomePage from "./pages/home";
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 const URL = "https://api.yelp.com/v3/businesses/search";
 
-const config = {
+let config = {
   headers: {
     Authorization:
       "Bearer EElj7yTkL-mqWepweO0Pgb-DQa_Yh9hsbvKPAKFB7dyFcDfAM5yn-a-0TH5I0eqbD8CA8RpbaIenoJ-Jw3Q33OOxz4mz6uct9H-ukaO7HpQXYYkV1Qd0TzDI95E4XXYx"
   },
   params: {
-    term: "bars",
+    term: "tacos",
     location: "Boston"
   }
 };
 
 class App extends Component {
-  myCallback = dataFromChild => {
-    config.params.term = dataFromChild;
-  };
-
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route
-            exact
-            path="/searchResults"
-            exact
-            strict
-            render={() => {
-              return (
-                <div>
-                  <SearchResultsNav />
-                  <Container>
-                    <Row className="justify-content-md-center">
-                      {this.renderMatches()}
-                    </Row>
-                  </Container>
-                  <footer>
-                    <div className="leftdiv">
-                      <a href="/" className="gh">
-                        Home
-                      </a>
-                      <a href="/about" className="gh">
-                        About
-                      </a>
-                      <a href="#" onclick="openSlideMenu()" className="gh">
-                        Favorites
-                      </a>
-                    </div>
-
-                    <div className="rightdiv">
-                      <a href="http://google.com" className="gh">
-                        Prev
-                      </a>
-                      <a href="http://google.com" className="gh">
-                        Next
-                      </a>
-                    </div>
-                  </footer>
-                </div>
-              );
-            }}
-          />
-          <Route exact path="/about" component={AboutPage} />
-        </Switch>
-      </Router>
-    );
-  }
-
   constructor() {
     super();
     this.state = {
-      data: []
+      data: [],
+      search: "",
+      headers: {
+        Authorization:
+          "Bearer EElj7yTkL-mqWepweO0Pgb-DQa_Yh9hsbvKPAKFB7dyFcDfAM5yn-a-0TH5I0eqbD8CA8RpbaIenoJ-Jw3Q33OOxz4mz6uct9H-ukaO7HpQXYYkV1Qd0TzDI95E4XXYx",
+        term: "tacos",
+        location: "Boston"
+      }
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    this.setState({ search: event.target.value });
   }
 
   componentDidMount() {
     axios
-      .get(proxyurl + URL, config)
+      .get(proxyurl + URL, params)
       .then(response => {
         this.setState({
           data: response.data.businesses,
@@ -134,6 +91,63 @@ class App extends Component {
         </Col>
       );
     });
+  }
+  openSlideMenu() {
+    return <div />;
+  }
+  render() {
+    console.log(this.state.headers);
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route
+            exact
+            path="/searchResults"
+            exact
+            strict
+            render={() => {
+              return (
+                <div>
+                  <SearchResultsNav
+                    value={this.state.search}
+                    handleSubmit={this.handleSubmit}
+                  />
+                  <Container>
+                    <Row className="justify-content-md-center">
+                      {this.renderMatches()}
+                    </Row>
+                  </Container>
+                  <footer>
+                    <div className="leftdiv">
+                      <a href="/" className="gh">
+                        Home
+                      </a>
+                      <a href="/about" className="gh">
+                        About
+                      </a>
+                      <a href="#" className="gh">
+                        Favorites
+                      </a>
+                    </div>
+
+                    <div className="rightdiv">
+                      <a href="http://google.com" className="gh">
+                        Prev
+                      </a>
+                      <a href="http://google.com" className="gh">
+                        Next
+                      </a>
+                    </div>
+                  </footer>
+                </div>
+              );
+            }}
+          />
+          <Route exact path="/about" component={AboutPage} />
+        </Switch>
+      </Router>
+    );
   }
 
   // render() {
--- src/SearchResultsNav.js
+++ src/SearchResultsNav.js
@@ -3,58 +3,54 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-class SearchResultsNav extends Component {
-  state = {};
-  render() {
-    return (
-      <nav>
-        <Container>
-          <form action="/action_page.php">
-            <Row className="justify-content-md-center">
-              <Col>
-                <div id="searchBar" className="col-lg text-center">
-                  <input
-                    type="text"
-                    name="search"
-                    placeholder="Search your craving"
-                  />
-                </div>
-              </Col>
-            </Row>
-            <Row className="justify-content-md-center">
-              <Col className="col-lg text-center">
-                <span>Distance:</span>
-                <br />
-                <input type="text" name="distance" placeholder="15mi" />
-              </Col>
-              <Col className="col-lg text-center">
-                <span>Rating:</span>
-                <br />
-                <select className="drop">
-                  <option value="1">✭</option>
-                  <option value="2">✭✭</option>
-                  <option value="3">✭✭✭</option>
-                  <option value="4">✭✭✭✭</option>
-                  <option value="5">✭✭✭✭✭</option>
-                </select>
-              </Col>
-              <Col className="col-lg text-center">
-                <span>Price:</span>
-                <br />
-                <select className="drop">
-                  <option value="1">$</option>
-                  <option value="2">$$</option>
-                  <option value="3">$$$</option>
-                  <option value="4">$$$$</option>
-                  <option value="5">$$$$$</option>
-                </select>
-              </Col>
-            </Row>
-          </form>
-        </Container>
-      </nav>
-    );
-  }
+export default function SearchResultsNav(props) {
+  return (
+    <nav>
+      <Container>
+        <form>
+          <Row className="justify-content-md-center">
+            <Col>
+              <div id="searchBar" className="col-lg text-center">
+                <input
+                  type="text"
+                  classnName={props.value}
+                  placeholder="Search your craving"
+                />
+              </div>
+            </Col>
+          </Row>
+          <Row className="justify-content-md-center">
+            <Col className="col-lg text-center">
+              <span>Distance:</span>
+              <br />
+              <input type="text" name="distance" placeholder="15mi" />
+            </Col>
+            <Col className="col-lg text-center">
+              <span>Rating:</span>
+              <br />
+              <select className="drop">
+                <option value="1">✭</option>
+                <option value="2">✭✭</option>
+                <option value="3">✭✭✭</option>
+                <option value="4">✭✭✭✭</option>
+                <option value="5">✭✭✭✭✭</option>
+              </select>
+            </Col>
+            <Col className="col-lg text-center">
+              <span>Price:</span>
+              <br />
+              <select className="drop">
+                <option value="1">$</option>
+                <option value="2">$$</option>
+                <option value="3">$$$</option>
+                <option value="4">$$$$</option>
+                <option value="5">$$$$$</option>
+              </select>
+            </Col>
+          </Row>
+          <button />
+        </form>
+      </Container>
+    </nav>
+  );
 }
-
-export default SearchResultsNav;
