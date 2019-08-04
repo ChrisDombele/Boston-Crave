aca58cbd8f66b99d1427dd8ea98331478e80fb3b
Charlie Pluk <charliepluk@users.noreply.github.com>
2019-08-04 18:26:31 -0400

    images on results

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,61 +1,93 @@
 import React from "react";
-import '../css/home.css';
+import "../css/home.css";
 
 // Photos used
-import Logo from '../images/LogoOpt.png';
+import Logo from "../images/LogoOpt.png";
 
 // Import Router for React
 import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    Link,
-    Redirect
-  } from "react-router-dom";
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from "react-router-dom";
 
 class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        data: [],
+        post: {
+          name: "",
+          radius: "",
+          rating: "",
+          price: "1",
+          limit: "15"
+        },
+        jobs: []
+      };
+    this.routeChange = this.routeChange.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
+  handleChange = e => {
+    const { name, value } = e.target;
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    this.setState(prevState => ({
+      post: { ...prevState.post, [name]: value }
+    }));
+  };
 
-    handleChange(event)
-    {
-        this.setState({value: event.target.value});
-    }
+  routeChange() {
+    let path = "/searchResults";
+    this.props.history.push(path);
+  }
 
-    handleSubmit(event) {
-        event.preventDefault();
-    }
-
-    render ()
-    {
-        return (
-            <div className="Home">
-                <section id="home" background-ratio="0.5">
-                    <div className="overlay"></div>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-offset-3 col-md-6.5 col-sm-12">
-                                <img src={Logo} className="img-responsive" alt="Crave Logo" height="390" width="600"/>
-                                <div className="home-info">
-                                    <form action="searchResults.html" method="get" className="online-form" onSubmit={this.handleSubmit}>
-                                        <input value={this.state.value} onChange={this.handleChange} type="text" name="text"className="form-control" placeholder="What's Your Crave?" required />
-                                        <button type="submit" className="form-control">Search</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-    
-                </section>
+  render() {
+    return (
+      <div className="Home">
+        <section id="home" background-ratio="0.5">
+          <div className="overlay" />
+          <div className="container">
+            <div className="row">
+              <div className="col-md-offset-3 col-md-6.5 col-sm-12">
+                <img
+                  src={Logo}
+                  className="img-responsive"
+                  alt="Crave Logo"
+                  height="390"
+                  width="600"
+                />
+                <div className="home-info">
+                  <form
+                    action="searchResults.html"
+                    method="get"
+                    className="online-form"
+                  >
+                    <input
+                      type="text"
+                      name="name"
+                      onChange={this.handleChange}
+                      className="form-control"
+                      placeholder="What's Your Crave?"
+                      required
+                    />
+                    <button
+                      type="submit"
+                      className="form-control"
+                      onClick={this.routeChange}
+                    >
+                      Search
+                    </button>
+                  </form>
+                </div>
+              </div>
             </div>
-        );
-    }
-};
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
 
 export default HomePage;
